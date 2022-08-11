d82d61b78fe7b196c8e8124613f2303d8014cb1d
Hamid Reza Bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-08-11 22:14:04 +0430

    stuck sorted array

---
+++ problem-solving/Almost_Sorted.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'almostSorted' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def almostSorted(arr):
+    # Write your code here
+    pass
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    almostSorted(arr)
