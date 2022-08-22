e6f5ec6d8211dbce0fc742dcbd24476bd44aa9e3
Hamid Reza Bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-08-22 15:13:39 +0430

    Mars_Exploration

--- problem-solving/Mars_Exploration.py
+++ problem-solving/Mars_Exploration.py
@@ -15,7 +15,15 @@ import sys
 
 def marsExploration(s):
     # Write your code here
-    pass
+    count = 0
+    for i in range(0,len(s),3):
+        if s[i] != 'S':
+            count += 1
+        if s[i+1] != 'O':
+            count += 1
+        if s[i+2] != 'S':
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
