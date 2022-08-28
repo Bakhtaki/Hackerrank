2a973d4b2496333548a140ff6a0b4a621da2d833
bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-08-28 23:11:28 +0430

    String Finished

---
+++ problem-solving/Alternating_Characters.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    deleted = 0
+
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            deleted += 1
+    return deleted
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
--- problem-solving/Gemstones.py
+++ problem-solving/Gemstones.py
@@ -12,7 +12,22 @@ import os
 
 def gemstones(arr):
     # Write your code here
-    pass
+    
+    minerals = {}
+    count = 0
+    
+    for element in arr:
+        element = list(set(element))
+        for each in element:
+            if each in minerals.keys():
+                minerals[each] += 1
+            else:
+                minerals[each] = 1
+    for _,value in minerals.items():
+        if value == len(arr):
+            count += 1
+    return count
+
 
 
 if __name__ == '__main__':
