07c0a4b047235546a308eba0e9a9225f3ca1c92b
bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-08-28 17:32:42 +0430

    Funny Strings

---
+++ problem-solving/Funny_string.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+import os
+
+#
+# Complete the 'funnyString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def funnyString(s):
+    # Write your code here
+    sr = s[::-1]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = funnyString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
