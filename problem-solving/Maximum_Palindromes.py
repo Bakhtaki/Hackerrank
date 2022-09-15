2dd2c1613d885e0ac5da143a21520a3f6cce9137
Hamid Reza Bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-09-15 18:10:01 +0430

    Maximum Palindrome

--- problem-solving/Maximum_Palindromes.py
+++ problem-solving/Maximum_Palindromes.py
@@ -1,34 +1,39 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
 
-#
-# Complete the 'initialize' function below.
-#
-# The function accepts STRING s as parameter.
-#
+maximum = 26
 
 
 def initialize(s):
-    # This function is called once before all queries.
-    pass
+    global arr, fac, mod, M
+    M = 1000000007
+    n = len(s)
+    arr = [[0 for _ in range(n + 1)] for _ in range(maximum)]
+    for i in range(n):
+        for j in range(maximum):
+            arr[j][i + 1] = arr[j][i] + ((ord(s[i]) - 97) == j)
+    fac = [1] * (n + 1)
+    mod = [1] * (n + 1)
+    for i in range(1, n + 1):
+        fac[i] = (fac[i - 1] * i) % M
+        mod[i] = pow(fac[i], M - 2, M)
 
-    #
-    # Complete the 'answerQuery' function below.
-    #
-    # The function is expected to return an INTEGER.
-    # The function accepts following parameters:
-    #  1. INTEGER l
-    #  2. INTEGER r
-    #
 
 def answerQuery(l, r):
-    # Return the answer for this query modulo 1000000007.
-    pass
+    global arr, fac, mod, M
+    odd = 0
+    even = 0
+    divs = 1
+    for i in range(maximum):
+        value = arr[i][r] - arr[i][l - 1]
+        odd += (value & 1)
+        even += (value // 2)
+        divs = (divs * mod[value//2]) % M
+    result = (fac[even] * divs) % M
+    if (odd > 0):
+        result = (result * odd) % M
+    return result
 
 
 if __name__ == '__main__':
