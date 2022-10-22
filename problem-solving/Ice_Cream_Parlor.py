02c4647d3f7a9379c0e94e1ecdac2039ff313dbf
bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-10-22 15:30:48 +0330

    IceCream Completed

---
+++ problem-solving/Ice_Cream_Parlor.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    budget = m
+    cost = arr
+
+    for i in range(len(cost)):
+        for j in range(i+1, len(cost)):
+            if cost[i] + cost[j] == budget:
+                return [i+1, j+1]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
+
