13001fcb721fb0384985f8621770b6ca8d582105
Hamid Reza Bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-11-08 19:00:46 +0330

    Making Candies

---
+++ problem-solving/Making_Candies.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumPasses' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. LONG_INTEGER m
+#  2. LONG_INTEGER w
+#  3. LONG_INTEGER p
+#  4. LONG_INTEGER n
+#
+
+
+def minimumPasses(m, w, p, n):
+    # Write your code here
+    pass
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    m = int(first_multiple_input[0])
+
+    w = int(first_multiple_input[1])
+
+    p = int(first_multiple_input[2])
+
+    n = int(first_multiple_input[3])
+
+    result = minimumPasses(m, w, p, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
