98ba78dba04f724d985ef62806369d928a6b7397
bakhtaki <hamidreza.bakhtaki@gmail.com>
2023-03-23 18:39:53 +0330

    Semi Edits

--- python/calendar_module.py
+++ python/calendar_module.py
@@ -1,11 +1,11 @@
-"""Calenar Module for the Calendar App."""
+"""Calendar Module for the Calendar App."""
 
 # -----------------------------------------------------------------------------
 # Task
 # You are given a date. Your task is to find what the day is on that date.
 
 # Input Format
-# A single line of input containing the space separated values of day, 
+# A single line of input containing the space separated values of day,
 # month and year respectively in the format dd mm yyyy.
 
 # Constraints
@@ -18,7 +18,7 @@
 # Output the correct day in capital letters.
 
 # Explanation
-# the day on 5 Agustus 2015 was "Wednesday"
+# the day on 5 Augusts 2015 was "Wednesday"
 
 # Sample Input
 # 05 08 2015
